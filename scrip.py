dc1435f651cc5b25e2d6c725b03657f88aa065ad
MoiLTD <moises.latorre.davila@gmail.com>
2025-02-04 21:03:40 -0500

    Primera actualización. archivo py y txt

---
+++ scrip.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+L=[3,5,10,3,5,4,5,8]
+
+media = np.mean(L)
+print("La media de la lista es:", media)
+
+
+lista = [12,54,24,2,34,3]
+
+promedio = sum(lista)/len(lista)
+
+print("La media de la lista 2 es:", promedio)
